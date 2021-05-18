ed3450dc78020b72e67a2dc7c696be4682c6fb8f
王昊 <229077035@qq.com>
2021-05-18 22:27:05 +0800

    update

--- MqttClient/Golang/Paho_emqx.go
+++ MqttClient/Golang/Paho_emqx.go
@@ -4,7 +4,9 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"os/signal"
+	"syscall"
+	//"time"
 
 	//import the Paho Go MQTT library
 	MQTT "github.com/eclipse/paho.mqtt.golang"
@@ -15,7 +17,35 @@ import (
 var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
 	fmt.Printf("TOPIC: %s\n", msg.Topic())
 	fmt.Printf("MSG: %s\n", msg.Payload())
+
+	if num < cap(array) {
+		array[num] = string(msg.Payload())
+	}else{
+		for i := 0; i <= cap(array)-2; i++ {
+			array[i] = array[i+1]
+		}
+		array[cap(array)-1] = string(msg.Payload())
+	}
+	num++
+	fmt.Printf("The number of received message: %d\n",num)
+	fmt.Printf("Received mssage: %v\n" ,array)
+
+
 }
+
+var connectHandler MQTT.OnConnectHandler = func(client MQTT.Client) {
+	fmt.Println("Connected")
+}
+
+var connectLostHandler MQTT.ConnectionLostHandler = func(client MQTT.Client, err error) {
+	fmt.Printf("Connect lost: %v", err)
+	client.Disconnect(250)
+
+}
+
+var num  = 0
+var array [6] string
+
 
 func main() {
 	//create a ClientOptions struct setting the broker address, clientid, turn
@@ -23,6 +53,10 @@ func main() {
 	opts := MQTT.NewClientOptions().AddBroker("tcp://172.16.167.92:1883")
 	opts.SetClientID("go_mqtt_client")
 	opts.SetDefaultPublishHandler(f)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+	opts.ConnectRetry = true
+	opts.AutoReconnect = true
 
 	//create and start a client using the above ClientOptions
 	c := MQTT.NewClient(opts)
@@ -30,16 +64,8 @@ func main() {
 		panic(token.Error())
 	}
 
-	//subscribe to the topic /go-mqtt/sample and request messages to be delivered
-	//at a maximum qos of zero, wait for the receipt to confirm the subscription
-	if token := c.Subscribe("ttt", 0, nil); token.Wait() && token.Error() != nil {
-		fmt.Println(token.Error())
-		os.Exit(1)
-	}
-
-
-
-	time.Sleep(99999 * time.Hour)
+	sub(c);
+	exit(c);
 
 	//unsubscribe from /go-mqtt/sample
 	//if token := c.Unsubscribe("application/3/device/53232c5e6c936483/event/#"); token.Wait() && token.Error() != nil {
@@ -48,4 +74,28 @@ func main() {
 	//}
 
 	//c.Disconnect(250)
+}
+
+func sub(client MQTT.Client) {
+	//subscribe to the topic /go-mqtt/sample and request messages to be delivered
+	//at a maximum qos of zero, wait for the receipt to confirm the subscription
+	if token := client .Subscribe("ttt", 0, nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+}
+
+func exit(clinet MQTT.Client){
+
+	// Messages will be delivered asynchronously so we just need to wait for a signal to shutdown
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+	signal.Notify(sig, syscall.SIGTERM)
+
+	<-sig
+	fmt.Println("signal caught - exiting")
+	clinet.Disconnect(1000)
+	fmt.Println("shutdown complete")
+
 }
