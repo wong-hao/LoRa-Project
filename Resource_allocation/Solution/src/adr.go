39047481114601dfa7d8422e0b7c54a1f2e5501c
hwan0171 <123>
2021-10-26 17:24:04 +0800

    update

--- Resource_allocation/Solution/src/adr.go
+++ Resource_allocation/Solution/src/adr.go
@@ -48,7 +48,7 @@ func defalutADR(dr int, txPower *float64, nbTrans *int)  {
 	//TODO: 看network-server的configuration里的disable_mac_commands=true是否会禁止ADR
 	//disable_adr=true或者disable_mac_commands=true后仍可以通过grpc发送MAC command
 	//TODO: 看confirmed message的确认是否会停止ADR设置的重传
-	//TODO: 看未设置ADR时confirmed message未收到ACK会不会导致重传 (实验中似乎出现过)
+	//TODO: 看未设置ADR时confirmed message未收到ACK会不会导致重传 (实验中似乎出现过)，属于ARQ
 	GRPC_Allocation(dr,txPowerIndex,*nbTrans)
 
 
