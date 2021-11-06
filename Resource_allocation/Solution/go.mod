18229faa2a29085853a49dff63305fb41e78ebae
hwan0171 <123>
2021-11-06 09:28:18 +0800

    update

--- Resource_allocation/Solution/go.mod
+++ Resource_allocation/Solution/go.mod
@@ -3,16 +3,16 @@ module Solution
 go 1.17
 
 require (
-	github.com/brocaar/chirpstack-api/go/v3 v3.12.3
-	github.com/brocaar/lorawan v0.0.0-20210809075358-95fc1667572e
+	github.com/brocaar/chirpstack-api/go/v3 v3.12.4
+	github.com/brocaar/lorawan v0.0.0-20211025085338-61c36dbfb10a
 	github.com/eclipse/paho.mqtt.golang v1.3.5
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/shopspring/decimal v1.2.0 // indirect
-	golang.org/x/net v0.0.0-20211020060615-d418f374d309 // indirect
-	golang.org/x/sys v0.0.0-20211023085530-d6a326fbbf70 // indirect
+	golang.org/x/net v0.0.0-20211105192438-b53810dc28af // indirect
+	golang.org/x/sys v0.0.0-20211105183446-c75c47738b0c // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20211021150943-2b146023228c // indirect
-	google.golang.org/grpc v1.41.0
+	google.golang.org/genproto v0.0.0-20211104193956-4c6863e31247 // indirect
+	google.golang.org/grpc v1.42.0
 )
 
 require (
--- Resource_allocation/Solution/src/gRPC_Allocation.go
+++ Resource_allocation/Solution/src/gRPC_Allocation.go
@@ -3,6 +3,7 @@
 //https://forum.chirpstack.io/t/method-to-send-downlink-mac-commands-to-end-node/3851
 //https://www.bilibili.com/video/BV1ee411p7zu?t=111
 //https://forum.chirpstack.io/t/the-linkadrreq-payload-enables-unexpected-uplink-channels/363/3
+//https://forum.chirpstack.io/t/adr-plugin-creation-example/11310/5
 
 package src
 
@@ -26,9 +27,9 @@ var (
 	//devEUI = lorawan.EUI64{0x53, 0x23, 0x2c, 0x5e, 0x6c, 0x93, 0x64, 0x83} //Rak811ABP
 	//devEUI = lorawan.EUI64{0xd9, 0x30, 0xad, 0xe2, 0x99, 0x58, 0x2a, 0xb5} //Rak811OTAA
 	//devEUI = lorawan.EUI64{0xc0, 0xe4, 0xec, 0xf4, 0xcd, 0x39, 0x9d, 0x55} //Rak4200ABP
-	devEUI = lorawan.EUI64{0x3d, 0xe0, 0x6c, 0x3b, 0x2b, 0x86, 0x70, 0x2a} //Rak4200OTAA
+	//devEUI = lorawan.EUI64{0x3d, 0xe0, 0x6c, 0x3b, 0x2b, 0x86, 0x70, 0x2a} //Rak4200OTAA
 	//devEUI = lorawan.EUI64{0x3b, 0xc1, 0xef, 0xb6, 0xe7, 0x19, 0xcc, 0x2c} //DraginoABP
-	//devEUI = lorawan.EUI64{0xd1, 0xdb, 0x29, 0x91, 0x91, 0x5b, 0x9e, 0x9e} //DraginoOTAA
+	devEUI = lorawan.EUI64{0x8b, 0xec, 0x4c, 0xec, 0x64, 0x0c, 0x7c, 0x2a} //DraginoOTAA
 )
 
 func GRPC_Allocation(datarate int, txpower int, Nbtrans int) {
@@ -49,16 +50,15 @@ func GRPC_Allocation(datarate int, txpower int, Nbtrans int) {
 
 	mac := lorawan.MACCommand{
 		CID: lorawan.LinkADRReq,
-		Payload: &lorawan.LinkADRReqPayload{ //TODO: 注意Service-profiles / Device-profiles里的对DataRate / TXPower的最大最小值设置
-			DataRate: uint8(datarate), //TODO: ADR Plugin通过函数HandleResponse获得的metadata TXPower
-			TXPower:  uint8(txpower),  //TODO: 如果不能通过integration获得txpower看看能不能通过Network Controller，实在不行就设定一个全局变量存储初始值，然后加一减一
+		Payload: &lorawan.LinkADRReqPayload{
+			DataRate:	uint8(datarate), //TODO: ADR Plugin通过函数HandleResponse获得的metadata TXPower
+			TXPower:	uint8(txpower), //TODO: 如果不能通过integration获得txpower看看能不能通过Network Controller，实在不行就设定一个全局变量存储初始值，然后加一减一
 			ChMask:		[16]bool{true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true},
-			Redundancy: lorawan.Redundancy{
-				//ChMaskCntl:	uint8(0),
-				NbRep: uint8(Nbtrans), //实测可以使用：接收到LinkAdrReq MAC指令后每次都会以3s间隔连续发送NbRep次，称之为重传；而每次重传发生终止的条件写在LoRaWAN Specification中
+			Redundancy:	lorawan.Redundancy{
+				ChMaskCntl:	uint8(0), //Channels 0 to 15
+				NbRep:		uint8(Nbtrans), //接收到LinkAdrReq MAC指令后每次都会以3s间隔连续发送NbRep次，称之为重传；而每次重传发生终止的条件写在LoRaWAN Specification中
 				//且重传的数据包fcnt相同，不会干扰到packet loss rate
-			}, //TODO: Golang空出来的输入默认值是多少（根据Live frame好像全是0）；对比MACexample以及其他人写的MAC看是否能够空出来那哪些值
-			//TODO: 看LoRaWAN Specification 1.0.2 中对Chmask、Redundancy的具体描述
+			},
 			//TODO: 看ADR Plugin如何写Go语言的ADR程序，以及最后迫不得已直接上Plugin不用MAC Command了
 
 		},
