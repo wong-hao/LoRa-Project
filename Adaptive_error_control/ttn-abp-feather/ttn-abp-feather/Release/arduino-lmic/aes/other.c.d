eab40fdeab94e123767471cd812097c8a4d636c6
hwan0171 <123>
2021-12-14 12:43:04 +0800

    update

---
+++ Adaptive_error_control/ttn-abp-feather/ttn-abp-feather/Release/arduino-lmic/aes/other.c.d
@@ -0,0 +1,9 @@
+C:\Users\229077035\AppData\Local\Temp\VMBuilds\ttn-abp-feather\uno\Release\arduino-lmic\aes\other.c.o: \
+ E:\ArduinoProjects\libraries\arduino-lmic\src\aes\other.c \
+ e:\arduinoprojects\libraries\arduino-lmic\src\lmic\oslmic.h \
+ e:\arduinoprojects\libraries\arduino-lmic\src\lmic\config.h \
+ e:\arduinoprojects\libraries\arduino-lmic\src\lmic\lmic_config_preconditions.h \
+ e:\arduinoprojects\libraries\arduino-lmic\project_config\lmic_project_config.h \
+ e:\arduinoprojects\libraries\arduino-lmic\src\lmic\lmic_env.h \
+ e:\arduinoprojects\libraries\arduino-lmic\src\lmic\oslmic_types.h \
+ e:\arduinoprojects\libraries\arduino-lmic\src\lmic\hal.h
---
+++ Adaptive_error_control/ttn-abp-feather/ttn-abp-feather/Release/arduino-lmic/hal/getpinmap_heltec_lora32.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\229077035\AppData\Local\Temp\VMBuilds\ttn-abp-feather\uno\Release\arduino-lmic\hal\getpinmap_heltec_lora32.cpp.o: \
+ E:\ArduinoProjects\libraries\arduino-lmic\src\hal\getpinmap_heltec_lora32.cpp
