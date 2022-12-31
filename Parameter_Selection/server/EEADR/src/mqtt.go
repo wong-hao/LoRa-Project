a67d694156497f33cb0551ddb498f12244103871
hwan0171 <123>
2022-12-31 19:43:04 +0800

    update

--- Parameter_Selection/server/EEADR/src/mqtt.go
+++ Parameter_Selection/server/EEADR/src/mqtt.go
@@ -37,7 +37,7 @@ const (
 	PASSWORD = "admin"
 
 	HISTORYCOUNT = 5  //Recent SNR history num
-	N            = 6  //Real number of GW
+	N            = 4  //Real number of GW
 	M            = 8  //Maximal number of ED (Do not change unless add more device)
 	Tinterval    = 10 //Transmission interval
 
